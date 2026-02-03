2c0c01cfe99a78ff1f74bc492b3b93970cf59176
Sarala-932 <msarala492@gmail.com>
2026-02-03 21:01:31 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Blog from './pages/Blog';
+import React from "react";
+import {Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+import Menu from "./pages/Menu";
+import BookTable from "./pages/BookTable";
+import Blog2 from "./pages/Blog2";
+
 export default function App() {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/blogs' element={<Blog />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/blogs" element={<Blog />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/book-table" element={<BookTable />} />
+        <Route path="/blogs/article" element={<Blog2 />} />
       </Routes>
     </div>
   );
